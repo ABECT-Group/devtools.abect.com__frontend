1840f092ff95babff3877abdc27e752d8d96852a
Roman Popovych <popovychjob@gmail.com>
2026-05-26 13:04:53 +0300

    add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react';
 
 hydrateRoot(
   document.getElementById('root'),
@@ -11,6 +12,7 @@ hydrateRoot(
     <HelmetProvider>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </HelmetProvider>
   </StrictMode>
